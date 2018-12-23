8d0033fbb82c7782396f180586ab6f7cb445a29a
Caiozed <caio.nevesleite@gmail.com>
2018-12-22 22:29:19 -0200

    Adicionar arquivos de projeto.

---
+++ BlazorTodoList/ViewModel/TodoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BlazorTodoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodoList.ViewModels
+{
+    public class TodoViewModel
+    {
+        public List<Todo> TodoList { get; set; }
+
+        public TodoViewModel()
+        {
+            TodoList = new List<Todo>();
+        }
+
+        public void AddTodo(string title, bool status = false)
+        {
+            TodoList.Add(new Todo { Title = title, IsDone = status });
+        }
+    }
+}
